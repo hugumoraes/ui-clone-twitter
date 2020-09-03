e11f06b4163379efc91e0a136e69a78c717bb5a2
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-09-03 15:53:21 -0300

    1st version completed

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import Button from '../Button';
+
+import {
+  Container,
+  Topside,
+  Logo,
+  MenuButton,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  Botside,
+  Avatar,
+  ProfileData,
+  ExitIcon
+} from './styles';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <Topside>
+        <Logo />
+
+        <MenuButton>
+          <HomeIcon />
+          <span>Página Inicial</span>
+        </MenuButton>
+
+        <MenuButton>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButton>
+
+        <MenuButton>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButton>
+
+        <MenuButton className="active">
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButton>
+
+        <MenuButton>
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </Topside>
+
+      <Botside>
+        <Avatar />
+
+        <ProfileData>
+          <strong>Hugo Moraes</strong>
+          <span>@hugumoraes</span>
+        </ProfileData>
+
+        <ExitIcon />
+      </Botside>
+    </Container>
+  );
+};
+
+export default MenuBar;
